a4bc0b3f4c741f59c851abfca66065c22e9c95e5
achhansrun <107165544+achhansrun@users.noreply.github.com>
2023-11-28 17:59:59 -0900

    Created test logo in blender and got opengl to render it (with issues)

--- Glitter/Glitter/Headers/glitter.hpp
+++ Glitter/Glitter/Headers/glitter.hpp
@@ -11,11 +11,15 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 // Reference: https://github.com/nothings/stb/blob/master/stb_image.h#L4
 // To use stb_image, add this in *one* C++ source file.
-//     #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+//#define STB_IMAGE_IMPLEMENTATION
+
 
 // Define Some Constants
 const int mWidth = 1280;
